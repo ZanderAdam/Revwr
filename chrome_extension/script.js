0ec0b65da639bc3ed71933206ca9605d5e27519a
aleks <aleks@alx.onl>
2023-03-15 22:44:26 -0400

    refac: move html/css to sep file

--- chrome_extension/script.js
+++ chrome_extension/script.js
@@ -28,7 +28,6 @@ function getReview(selectedCode) {
   })
     .then(response => response.json())
     .then(data => {
-      console.log(data);
       var displayElem = document.getElementById('parsed-lines');
       displayElem.innerHTML = data.review;
     })
@@ -39,61 +38,61 @@ function getReview(selectedCode) {
 
 function toggleSidebar() {
   if (sidebarOpen) {
+    console.log('closing sidebar');
     var el = document.getElementById('side-panel');
     el.parentNode.removeChild(el);
+
+    document.body.style.cssText = "margin-right: 0px;";
+
     sidebarOpen = false;
   } else {
-    let codeLines = document.querySelectorAll('.diff-table tr');
-    for (let i = 0; i < codeLines.length; i++) {
-      let selElem = document.createElement('input');
-      selElem.type = 'checkbox';
-      selElem.className = 'line-selector';
-      selElem.style.cssText = 'position: absolute;left: 0;';
-      selElem.addEventListener('mouseover', selectLine);
-      codeLines[i].appendChild(selElem);
-    }
+    console.log('opening sidebar');
+    fetch(chrome.runtime.getURL("template.html"))
+      .then(response => response.text())
+      .then(data => {
+        console.log(data);
+        let parser = new DOMParser();
+        let templateContent = parser.parseFromString(data, "text/html");
+        let parseButton = templateContent.querySelector('#parse-button');
+        let parsedLines = templateContent.querySelector('#parsed-lines');
 
-    let sidePanel = document.createElement('div');
-    let parseButton = document.createElement('button');
-    let parsedLines = document.createElement('div');
+        let codeLines = document.querySelectorAll('.diff-table tr');
+        for (let i = 0; i < codeLines.length; i++) {
+          let selElem = document.createElement('input');
+          selElem.type = 'checkbox';
+          selElem.className = 'line-selector';
+          selElem.style.cssText = 'position: absolute;left: 0;';
+          selElem.addEventListener('mouseover', selectLine);
+          codeLines[i].appendChild(selElem);
+        }
 
-    sidePanel.id = 'side-panel';
-    parseButton.id = 'parse-button';
-    parsedLines.id = 'parsed-lines';
+        parseButton.addEventListener('click', () => {
+          let selectedLines = [];
+          let rowSelectors = document.querySelectorAll('.line-selector:checked');
+          rowSelectors.forEach(sel => {
+            var row = sel.parentNode;
+            var dataElem = row.querySelector('.blob-code .add-line-comment');
+            console.log(dataElem.attributes['data-original-line']);
+            selectedLines.push(dataElem.attributes['data-original-line'].value);
+          });
+          parsedLines.innerHTML = selectedLines.join('<br>');
+          getReview(selectedLines.join('\n'));
+        });
 
-    sidePanel.style.cssText =
-      "position: fixed; top: 0; \
-        right: 0; width: 300px; height: 100%; \
-        background-color: #f1f1f1; \
-        padding: 20px; box-sizing: border-box; \
-        overflow-y: scroll; z-index:9999999999; \
-        color: black";
+        document.body.appendChild(templateContent.firstChild);
 
-    parseButton.innerHTML = 'Explain';
-    sidePanel.appendChild(parseButton);
-    sidePanel.appendChild(parsedLines);
-    document.body.appendChild(sidePanel);
+        document.body.style.cssText = "margin-right: 300px;";
 
-    parseButton.addEventListener('click', () => {
-      let selectedLines = [];
-      let rowSelectors = document.querySelectorAll('.line-selector:checked');
-      rowSelectors.forEach(sel => {
-        var row = sel.parentNode;
-        var dataElem = row.querySelector('.blob-code .add-line-comment');
-        console.log(dataElem.attributes['data-original-line']);
-        selectedLines.push(dataElem.attributes['data-original-line'].value);
+        sidebarOpen = true;
+      })
+      .catch(error => {
+        console.error(error);
       });
-      parsedLines.innerHTML = selectedLines.join('<br>');
-      getReview(selectedLines.join('\n'));
-    });
-    document.body.appendChild(sidePanel);
-    sidebarOpen = true;
   }
 }
 
 document.body.onmousedown = function (event) {
   var element = event.target;
-  console.log(element);
   if (element.classList.contains('line-selector')) {
     console.log('selecting');
     element.checked = true;
